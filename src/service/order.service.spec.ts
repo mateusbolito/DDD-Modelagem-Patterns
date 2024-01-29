1f043bcc6fde5499751d0f69a360d23392b6ff2f
Mateus Bolito <mateusbolito@hotmail.com>
2024-01-28 22:17:57 -0300

    Rewward points

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,19 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
 import OrderService from "./order.service";
 
 describe("orderService unit test", () => {
+  it("should place an order", () => {
+    const customer = new Customer("c1", "Customer 1");
+    const item1 = new OrderItem("i1", "item1", 10, "p1", 1);
+
+    const order = OrderService.placeOrder(customer, [item1]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total().toBe(10));
+  });
+
   it("should get total off all orders", () => {
     const item1 = new OrderItem("1", "Item1", 100, "p1", 1);
     const item2 = new OrderItem("1", "Item1", 100, "p1", 1);
