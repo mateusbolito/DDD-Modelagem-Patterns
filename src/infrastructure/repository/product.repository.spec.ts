09a4814c2333fede2c30ccd1a7c8047ed1977a75
Mateus Bolito <mateusbolito@hotmail.com>
2024-01-30 15:35:20 -0300

    repository product create check

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,36 +1,25 @@
-import { DataTypes, Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
+
+import ProductRepository from "./product.repository";
 import ProductModel from "../db/sequelize/model/product.model";
 import Product from "../../domain/entity/product";
-import ProductRepository from "./product.repository";
-import { DataType } from "sequelize-typescript";
 
-describe("Product repository tests", () => {
-  let sequelize: Sequelize;
+describe("Product repository test", () => {
+  let sequileze: Sequelize;
 
   beforeEach(async () => {
-    sequelize = new Sequelize({
+    sequileze = new Sequelize({
       dialect: "sqlite",
       storage: ":memory:",
       logging: false,
       sync: { force: true },
     });
-    sequelize.define<ProductModel>("Product", {
-      id: {
-        primaryKey: true,
-        type: DataTypes.INTEGER.UNSIGNED,
-      },
-      name: {
-        type: DataType.STRING,
-      },
-      price: {
-        type: DataTypes.NUMBER,
-      },
-    });
-
-    await sequelize.sync();
+    sequileze.addModels([ProductModel]);
+    await sequileze.sync();
   });
+
   afterEach(async () => {
-    await sequelize.close();
+    await sequileze.close();
   });
 
   it("should create a product", async () => {
@@ -46,5 +35,64 @@ describe("Product repository tests", () => {
       name: "Product 1",
       price: 100,
     });
+  });
+
+  it("should update a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+
+    await productRepository.create(product);
+
+    const productModel = await ProductModel.findOne({ where: { id: "1" } });
+
+    expect(productModel?.toJSON()).toStrictEqual({
+      id: "1",
+      name: "Product 1",
+      price: 100,
+    });
+
+    product.changeName("Product 2");
+    product.changePrice(200);
+
+    await productRepository.update(product);
+
+    const productModel2 = await ProductModel.findOne({ where: { id: "1" } });
+
+    expect(productModel2?.toJSON()).toStrictEqual({
+      id: "1",
+      name: "Product 2",
+      price: 200,
+    });
+  });
+
+  it("should find a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+
+    await productRepository.create(product);
+
+    const productModel = await ProductModel.findOne({ where: { id: "1" } });
+
+    const foundProduct = await productRepository.find("1");
+
+    expect(productModel?.toJSON()).toStrictEqual({
+      id: foundProduct.id,
+      name: foundProduct.name,
+      price: foundProduct.price,
+    });
+  });
+
+  it("should find all products", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+    await productRepository.create(product);
+
+    const product2 = new Product("2", "Product 2", 200);
+    await productRepository.create(product2);
+
+    const foundProducts = await productRepository.findAll();
+    const products = [product, product2];
+
+    expect(products).toEqual(foundProducts);
   });
 });
--- src/infrastructure/repository/product.repository.ts
+++ src/infrastructure/repository/product.repository.ts
@@ -10,14 +10,31 @@ export default class ProductRepository implements ProductRepositoryInterface {
       price: entity.price,
     });
   }
+
   async update(entity: Product): Promise<void> {
-    throw new Error("Method not implemented");
+    await ProductModel.update(
+      {
+        name: entity.name,
+        price: entity.price,
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      }
+    );
   }
+
   async find(id: string): Promise<Product> {
-    throw new Error("Method not implemented");
+    const productModel: any = await ProductModel.findOne({ where: { id } });
+    return new Product(productModel.id, productModel.name, productModel.price);
   }
 
   async findAll(): Promise<Product[]> {
-    throw new Error("Method not implemented");
+    const productModels = await ProductModel.findAll();
+    return productModels.map(
+      (productModel: { id: any; name: any; price: any }) =>
+        new Product(productModel.id, productModel.name, productModel.price)
+    );
   }
 }
